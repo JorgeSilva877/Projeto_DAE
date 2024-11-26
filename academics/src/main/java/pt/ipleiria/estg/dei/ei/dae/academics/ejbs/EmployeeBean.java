9a6742ded40276c96d3a24b1891e77be6e803f17
João Francisco Matias Couto <joaofrei04@gmail.com>
2024-11-26 16:56:06 +0000

    Beans

--- academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/EmployeeBean.java
+++ academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/EmployeeBean.java
@@ -1,4 +1,18 @@
 package pt.ipleiria.estg.dei.ei.dae.academics.ejbs;
 
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.academics.entities.Employee;
+import pt.ipleiria.estg.dei.ei.dae.academics.entities.Manager;
+
+@Stateless
 public class EmployeeBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void create(String username, String password, String name, String email){
+        var employee = new Employee(username, password, name, email);
+        entityManager.persist(employee);
+    }
 }
--- academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/ProductBean.java
+++ academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/ProductBean.java
@@ -1,4 +1,34 @@
 package pt.ipleiria.estg.dei.ei.dae.academics.ejbs;
 
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.academics.entities.Product;
+
+import java.util.List;
+
+@Stateless
 public class ProductBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public void create(long code, String name, String category, String estado, int stock, double price) {
+        var product = new Product( code,  name,  category,  estado,  stock,  price);
+        entityManager.persist(product);
+    }
+
+    public List<Product> findAll() {
+        // remember, maps to: “SELECT s FROM Student s ORDER BY s.name”
+        return entityManager.createNamedQuery("getAllProducts", Product.class).getResultList();
+    }
+
+    public Product find(long code) {
+        var produto = entityManager.find(Product.class, code);
+        if (produto == null) {
+            throw new RuntimeException("produto " + code + " not found");
+        }
+        return produto;
+    }
 }
