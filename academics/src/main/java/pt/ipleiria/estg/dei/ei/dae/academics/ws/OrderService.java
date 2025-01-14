6eabe7334fd3964d3f7c9179cdfc8035f550d9e5
Jorgin877 <2210998@my.ipleiria.pt>
2025-01-14 10:23:24 +0000

    Orders Working? (nao da pa testar ainda)

--- academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ws/OrderService.java
+++ academics/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ws/OrderService.java
@@ -3,8 +3,12 @@ package pt.ipleiria.estg.dei.ei.dae.academics.ws;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import pt.ipleiria.estg.dei.ei.dae.academics.dtos.OrderDTO;
+import pt.ipleiria.estg.dei.ei.dae.academics.dtos.ProductDTO;
 import pt.ipleiria.estg.dei.ei.dae.academics.ejbs.OrderBean;
+import pt.ipleiria.estg.dei.ei.dae.academics.entities.Order;
+import pt.ipleiria.estg.dei.ei.dae.academics.entities.Product;
 import pt.ipleiria.estg.dei.ei.dae.academics.exceptions.MyEntityNotFoundException;
 
 import java.util.List;
@@ -25,14 +29,18 @@ public class OrderService {
 
     @POST
     @Path("/")
-    public void create(OrderDTO orderDTO) throws MyEntityNotFoundException {
+    public Response create(OrderDTO orderDTO) throws MyEntityNotFoundException {
         orderBean.create(
                 orderDTO.getCode(),
                 orderDTO.getClient(),
                 orderDTO.getMorada(),
                 orderDTO.getPrecoTotal(),
-                orderDTO.getProducts()
-
+                orderDTO.getProductsId()
         );
+
+        Order order = orderBean.find(orderDTO.getCode());
+        return Response.status(Response.Status.CREATED)
+                .entity(OrderDTO.from(order))
+                .build();
     }
 }
